bfa08e6bfd779af5410c1e9d2adb8e55bbcaed0f
AngelHerradaP <aherpor456@g.educaand.es>
2024-11-05 14:14:21 +0100

    a

--- eclipse-workspace/r03/src/prog/unidad03/repeticion/InvierteNumero.java
+++ eclipse-workspace/r03/src/prog/unidad03/repeticion/InvierteNumero.java
@@ -1,6 +1,5 @@
 package prog.unidad03.repeticion;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class InvierteNumero {
@@ -9,17 +8,40 @@ public class InvierteNumero {
     
     Scanner sc = new Scanner(System.in);
     
-    int resto = 0;
-    int numeroInvertido = resto;
-    
     System.out.println("Introduce un numero entero positivo: ");
     int numeroIntroducido = Integer.parseInt(sc.nextLine());
-    
-    while (numeroIntroducido > 0) {
 
+    int multiplicador = 1;
+    int numeroInvertido = 0;
+    int numeroIntroducidoCp = numeroIntroducido;
+    int numeroFinal = 0;
+    
+    if (numeroIntroducido > 0) {
       
+      for (int i = 9; i < numeroIntroducido; i = i * 10 + 9) {
+        
+        multiplicador *= 10;
+      }
+      
+      for (;multiplicador >= 1; multiplicador /= 10) {
+
+        int ultimoDigito = numeroIntroducidoCp % 10;
+        numeroInvertido = ultimoDigito * multiplicador;
+        numeroFinal = numeroFinal + numeroInvertido;
+        numeroIntroducidoCp =  numeroIntroducidoCp / 10;
+        
+      } 
+      
+      System.out.printf("El resultado de invertir el numero %d es: %d", numeroIntroducido, numeroFinal);
+      
+    }else if (numeroIntroducido == 0) {
+      
+      System.out.println("El resultado de invertir el numero 0 es: 0");
+      
+    }else {
+      
+      System.out.println("Introduce un numero entero positivo");
       
     }
-    
   }
 }
